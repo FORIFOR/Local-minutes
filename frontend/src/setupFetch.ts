9b0d0430da1cb22837f16a4644bd7b8edfb3bb82
堀尾周平 <horioshuuhei@horioshuuheinoMac-mini.local>
2025-11-16 23:37:15 +0900

    Fix Google Login

--- frontend/src/setupFetch.ts
+++ frontend/src/setupFetch.ts
@@ -2,9 +2,14 @@ const originalFetch = window.fetch.bind(window)
 
 window.fetch = (input: RequestInfo | URL, init?: RequestInit) => {
   const finalInit: RequestInit = init ? { ...init } : {}
-  if (!finalInit.credentials) {
-    finalInit.credentials = 'include'
+  finalInit.credentials = finalInit.credentials || 'include'
+
+  const token = localStorage.getItem('accessToken')
+  if (token) {
+    finalInit.headers = {
+      ...(finalInit.headers || {}),
+      Authorization: `Bearer ${token}`,
+    }
   }
   return originalFetch(input, finalInit)
 }
-
